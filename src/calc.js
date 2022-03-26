e25f6d7b280ac261b6c92fbacaca25af41f1f605
William Chen <10979111@gm.nfu.edu.tw>
2022-03-26 09:30:30 +0800

    complete initial functionality, begin to develop customer filter functionality

---
+++ src/calc.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react";
+import "./calc.css";
+
+export default class Calc extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateTime: "",
+      customer: "",
+      spec: "",
+      quantity: 0,
+      bucketReturned: 0,
+    };
+  }
+
+  get customer() {
+    return (
+      <div className="customer">
+        <label htmlFor="customer" className="customer-title">
+          客戶姓名:{" "}
+        </label>
+        <input
+          type="text"
+          placeholder="客戶姓名"
+          id="customer"
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+  get currentTime() {
+    const today = new Date();
+    const date = [
+      today.getFullYear(),
+      today.getMonth() + 1,
+      today.getDate(),
+    ].join("/");
+    const time = [
+      String(today.getHours()).padStart(2, "0"),
+      String(today.getMinutes()).padStart(2, "0"),
+      String(today.getSeconds()).padStart(2, "0"),
+    ].join(":");
+    return [date, time].join(" ");
+  }
+
+  get dateTime() {
+    return (
+      <div className="date">
+        <span className="date-title">日期: </span>
+        {this.currentTime}
+      </div>
+    );
+  }
+
+  get spec() {
+    const options = [
+      { value: "default", label: "請選擇規格" },
+      { value: "脆皮", label: "脆皮豆腐" },
+      { value: "傳統", label: "傳統豆腐" },
+      { value: "串燒", label: "串燒豆腐" },
+      { value: "大", label: "大規格豆腐" },
+      { value: "特大", label: "特大規格豆腐" },
+    ];
+    return (
+      <div className="spec">
+        <label className="spec-title">規格: </label>
+        <select onChange={this.handleChange} id="spec">
+          {options.map((option) => (
+            <option key={option.label} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+  get quantity() {
+    return this.generateNumberInputFiled("購買數量", "quantity");
+  }
+
+  get bucketReturned() {
+    return this.generateNumberInputFiled("退桶數量", "bucketReturned");
+  }
+  generateNumberInputFiled(label, fieldName) {
+    return (
+      <div className={fieldName}>
+        <label htmlFor={fieldName} className={`${fieldName}-title`}>
+          {label}:{" "}
+        </label>
+        <input
+          type="number"
+          id={fieldName}
+          onChange={this.handleChange}
+          placeholder={`請輸入${label}`}
+        />
+      </div>
+    );
+  }
+  get purchasePrice() {
+    const itemPriceLookup = {
+      脆皮: 780,
+      傳統: 840,
+      串燒: 730,
+      大: 800,
+      特大: 830,
+    };
+    const { spec, quantity, bucketReturned } = this.state;
+    let totalPrice = itemPriceLookup[spec] * +quantity - +bucketReturned * 100;
+    if (Number.isNaN(totalPrice)) {
+      totalPrice = 0;
+    }
+
+    return (
+      <div>
+        <span className={"purchasePrice-title"}>總售價: </span>
+        {totalPrice != 0 ? totalPrice : null}
+      </div>
+    );
+  }
+
+  handleChange = (evt) => {
+    const currentTargetValue = evt.target.value;
+    if (currentTargetValue < 0) {
+      return;
+    }
+
+    this.setState({
+      [evt.target.id]: currentTargetValue,
+    });
+  };
+
+  componentDidMount = () => {
+    setInterval(() => {
+      this.setState((prevState) => ({ dateTime: this.currentTime }));
+    }, 1000);
+  };
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div className="calc">
+        <p className="title">訂單計算機</p>
+        {this.dateTime}
+        {this.customer}
+        {this.spec}
+        {this.quantity}
+        {this.bucketReturned}
+        {this.purchasePrice}
+      </div>
+    );
+  }
+}
